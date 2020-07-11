02f137bb54b7d542a7a3b8f884ce7994d441370d
Travis Fox <travis.fox90@gmail.com>
2020-07-11 19:59:56 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_array = self.split(/[.?!]/)
+    full_sentence_array = sentence_array.select do |x|
+      x.length > 0
+    end
+  
+    if full_sentence_array.size == 0
+      0
+    else
+      full_sentence_array.size
+    end
+    
   end
 end
